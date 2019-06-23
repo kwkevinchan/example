1c69c3526542849b60dbdfbd944db53702d09074
kwkevin <kwkevin.chan@gmail.com>
2019-06-23 15:11:31 +0800

    create

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,6 +10,7 @@
             <h5 class="font-weight-bold flex-grow-1 " style="margin-top:5px">
               使用者清單
             </h5>
+            @include('components.popModal')
 
             <form class="ml-auto col-md-6">
               <div class="form-group" style="margin-bottom:0px">
@@ -58,9 +59,37 @@
         table.push('<td>' + d['birthday'] + '</td>')
         table.push('<td>' + d['comment'] + '</td>')
         table.push('<td><a href="/user/'+ d['id'] +'/edit" class="btn text-white btn-primary" role="button" aria-pressed="true">修改使用者</a></td>')
-        table.push('<td><a href="/user/'+ d['id'] +'/delete" class="btn text-white btn-primary" role="button" aria-pressed="true">刪除使用者</a></td>')
+        table.push('<td><a href="/user/'+ d['id'] +'/delete" class="btn text-white btn-primary" role="button" aria-pressed="true" onclick="delete(' + d['id'] + ',' + d['name'] +')">刪除使用者</a></td>')
       table.push('</tr>')
   }
   document.getElementById('mainTable').innerHTML = table.join('');
+
+  function delete(id, name) {
+    title = document.getElementById('popModalTitle');
+    title.innerHTML = ('您將刪除:' + name);
+    url = document.getElementById('popModalUrl');
+    url.setAttribute('onclick', "sendDelete("+id+")");
+    $('#popModal').modal('show');
+  }
+
+  function sendDelete(id) {
+    axios.post("{{ route('user.delete') }}")
+        .then(function(res) {
+            console.log(res.data.msg);
+            title = document.getElementById('popModalTitle');
+            title.innerHTML = (res.data.status);
+            body = document.getElementById('popModalBody');
+            body.innerHTML = (res.data.msg);
+            $('#popModal').modal('show');
+        })
+        .catch(function(error) {
+            console.log(error.response.data);
+            title = document.getElementById('popModalTitle');
+            title.innerHTML = (error.response.data.status);
+            body = document.getElementById('popModalBody');
+            body.innerHTML = (error.response.data.msg);
+            $('#popModal').modal('show');
+        });
+  }
 </script>
 @endsection
